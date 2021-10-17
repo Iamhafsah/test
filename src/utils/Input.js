a3bffed5d36a0540818ce7b4dd59657a59c1dd1f
hafsah <emekomahafsah@gmail.com>
2021-10-17 22:21:04 +0100

    modifications to page and styles

--- src/utils/Input.js
+++ src/utils/Input.js
@@ -1,12 +1,14 @@
 const Input = ({label, text, social, extraStyle, inputValue, setInputValue, optional}) => {
 const empty = inputValue === '';
 
-   const handleChange = (e) => setInputValue(e.target.value)
+   const handleChange = (e) => setInputValue(e.target.value)          
+        const emptyInput = empty ? "empty-input" : "";
+
     return (
         <div>
             <label htmlFor={label}>{text}</label> <br/> 
-            {empty && <span className="empty-input" >!</span>}
-            <input type="text" className={`${extraStyle} account-input` }  placeholder={social ? "@" : ""} value={inputValue} onChange={!optional && handleChange} required /> <br/>
+            
+            <input type="text" className={`${extraStyle} ${emptyInput} account-input` }  placeholder={social ? "@" : ""} value={inputValue} onChange={!optional && handleChange} required /> <br/>
         </div>
     )
 }
